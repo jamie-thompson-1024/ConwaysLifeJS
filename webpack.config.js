8b12aad498fc64afecebedff3fce9b16e02961e2
sophisticated-snake <jamie.thompson.1024@gmail.com>
2022-02-15 22:16:16 +1300

    basic setup

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+
+module.exports = {
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  output: {
+    filename: 'conwaylife.js',
+    library: 'conwaylife',
+    libraryTarget: 'umd',
+    path: './dist',
+  },
+};
