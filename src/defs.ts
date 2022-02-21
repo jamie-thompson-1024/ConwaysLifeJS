2ae989422267c6b98d58bc189a9c3f6291873c99
sophisticated-snake <jamie.thompson.1024@gmail.com>
2022-02-21 15:11:11 +1300

    Create Definitions

--- src/defs.ts
+++ src/defs.ts
@@ -1,1 +1,14 @@
 
+type EdgeMode = 'wrap' | 'empty' | 'filled';
+
+interface Options
+{
+    width?: number;
+    height?: number;
+    tickDelay?: number;
+    edgeMode?: EdgeMode;
+};
+
+export type { 
+    Options, EdgeMode 
+};
