54b76e4821aad885eee228d0cb90cae31517c4e8
sophisticated-snake <jamie.thompson.1024@gmail.com>
2022-02-22 16:27:39 +1300

    Update test page

--- public/index.js
+++ public/index.js
@@ -1,9 +1,46 @@
 
 const life = new ConwaysLife({
-    edgeMode: 'filled'
+    edgeMode: 'filled',
+    height: 50,
+    width: 50
 });
 console.log(life);
 
-life.step();
+let container = document.getElementById('canvasContainer');
+let canvas = document.getElementById('canvas');
+let ctx = canvas.getContext('2d');
 
-console.log(life.grid);
+resize();
+
+function draw()
+{
+    let squareWidth = canvas.width / life.width;
+    let squareHeight = canvas.height / life.height;
+    
+    ctx.fillStyle = '#000000';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    ctx.fillStyle = '#FEFEFE';
+    life.grid.forEach((row, y) => {
+        row.forEach((pixel, x) => {
+            if(pixel)
+                ctx.fillRect(
+                    x * squareWidth,
+                    y * squareHeight,
+                    squareWidth,
+                    squareHeight);
+        });
+    });
+}
+
+function resize()
+{
+    canvas.width = container.clientWidth;
+    canvas.height = container.clientHeight;
+}
+
+addEventListener('resize', () => { resize() });
+
+life.onTick = () => { draw() };
+
+life.run();
