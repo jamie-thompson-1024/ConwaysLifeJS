00515fd80c08e49b7160b8f63830a678e4220937
sophisticated-snake <jamie.thompson.1024@gmail.com>
2022-03-02 22:31:42 +1300

    Change demo port

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const app = express();
 app.use(express.static('public'));
 app.use(express.static('dist'));
 
-app.listen(3000);
+app.listen(8899);
 
-console.log("Page served at: localhost:3000");
+console.log("Page served at: localhost:8899");
