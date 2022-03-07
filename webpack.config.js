a88319b74e10729be5c9b772cdfdb2d611adf6da
sophisticated-snake <jamie.thompson.1024@gmail.com>
2022-03-07 17:02:48 +1300

    rebuilt in production mode

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const TerserPlugin = require("terser-webpack-plugin");
 
 const generalConfig = {
-  mode: 'development',
+  mode: 'production',
   entry: './src/ConwaysLife.ts',
   module: {
     rules: [
